a1df1f715201666acad9b64faa506df2beb73b6a
Yazdan <051iranmashhad@gmail.com>
2026-02-10 03:21:38 +0330

    - replace `MainActivity` launch with `SplashFragment` in `AppActivity` - remove unused imports in `AppActivity`

--- app/src/main/java/ir/pakbanmanagement/presentation/main/activity/AppActivity.kt
+++ app/src/main/java/ir/pakbanmanagement/presentation/main/activity/AppActivity.kt
@@ -3,18 +3,14 @@ package ir.pakbanmanagement.presentation.main.activity
 import dagger.hilt.android.AndroidEntryPoint
 import ir.pakbanmanagement.databinding.ActivityAppBinding
 import ir.pakbanmanagement.other.BaseActivity
-import ir.pakbanmanagement.other.launchActivityAndFinish
 import ir.pakbanmanagement.other.setNavigator
-import ir.pakbanmanagement.presentation.main.fragment.HomeFragment
 import ir.pakbanmanagement.presentation.main.fragment.SplashFragment
 
 @AndroidEntryPoint
 class AppActivity : BaseActivity<ActivityAppBinding>() {
 
     override fun setOnView() {
-//        setNavigator(SplashFragment())
-
-        launchActivityAndFinish<MainActivity>()
+        setNavigator(SplashFragment())
     }
 
     override fun getViewBindingInflater(): ActivityAppBinding {
